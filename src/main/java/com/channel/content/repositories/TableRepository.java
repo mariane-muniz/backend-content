47d4f4a376aaad643069acaf211305cedfd2eb50
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2019-10-13 19:16:20 -0300

    Add search by system type

--- src/main/java/com/channel/content/repositories/TableRepository.java
+++ src/main/java/com/channel/content/repositories/TableRepository.java
@@ -3,8 +3,10 @@ package com.channel.content.repositories;
 import com.channel.content.models.Table;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "table", path = "tables")
 public interface TableRepository extends CrudRepository<Table, Long> {
+    public Table findOneByClassName(@Param("className") String className);
 }
