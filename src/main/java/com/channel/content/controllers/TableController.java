62ddcdada4ca728328fc55009bdd3a75370f4766
mariane-muniz <39652078+mariane-muniz@users.noreply.github.com>
2019-10-26 12:51:35 -0300

    Update TableController.java

--- src/main/java/com/channel/content/controllers/TableController.java
+++ src/main/java/com/channel/content/controllers/TableController.java
@@ -17,7 +17,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(
+    origins = "*",
+    allowedHeaders = "*",
+    allowCredentials = "false"
+)
 @RestController
 @RequestMapping("/list")
 public class TableController {
